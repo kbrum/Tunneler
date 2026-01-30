2a2ff19a5b8cb5bd0280e832a1a6ccea914aebca
kayky <kaykyazevedo97@gmail.com>
2026-01-30 01:35:28 -0300

    feat: servers sidebar tree view

--- frontend/src/features/dashboard/components/LogoutAlertDialog.tsx
+++ frontend/src/features/dashboard/components/LogoutAlertDialog.tsx
@@ -1,6 +1,5 @@
 import {
   AlertDialog,
-  AlertDialogAction,
   AlertDialogCancel,
   AlertDialogContent,
   AlertDialogHeader,
@@ -11,20 +10,24 @@ import {Button} from '@/components/ui/button';
 import {Spinner} from '@/components/ui/spinner';
 import {useLogout} from '@/hooks/use-session';
 import {LogOut} from 'lucide-react';
+import {useState} from 'react';
 import {useNavigate} from 'react-router-dom';
 
 export function LogoutAlertDialog() {
   const {logout, isLoadingLogout} = useLogout();
+  const [isOpen, setIsOpen] = useState(false);
   const navigate = useNavigate();
 
-  const handleLogout = async () => {
+  const handleLogout = async (e: React.MouseEvent) => {
+    e.preventDefault();
     await logout();
+    setIsOpen(false);
     navigate('/login');
   };
 
   return (
-    <AlertDialog>
-      <AlertDialogTrigger>
+    <AlertDialog open={isOpen} onOpenChange={setIsOpen}>
+      <AlertDialogTrigger asChild>
         <Button variant="ghost" className="h-8 w-30 p-0">
           <LogOut className="h-4 w-4" />
           <span>Log out</span>
@@ -34,14 +37,16 @@ export function LogoutAlertDialog() {
         <AlertDialogHeader>
           <AlertDialogTitle>Are you sure you want to log out?</AlertDialogTitle>
           <div className="flex flex-row justify-end gap-3 pt-4">
-            <AlertDialogCancel>Cancel</AlertDialogCancel>
-            <AlertDialogAction
+            <AlertDialogCancel disabled={isLoadingLogout}>
+              Cancel
+            </AlertDialogCancel>
+            <Button
               className="hover:bg-red-600 hover:text-white"
               disabled={isLoadingLogout}
               onClick={handleLogout}
             >
               {isLoadingLogout ? <Spinner /> : 'Logout'}
-            </AlertDialogAction>
+            </Button>
           </div>
         </AlertDialogHeader>
       </AlertDialogContent>
